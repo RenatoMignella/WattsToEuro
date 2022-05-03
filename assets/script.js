295898f351f67b20e1927672b7a9ab1b86ccbcd7
Renato Mignella <renatoferigato@gmail.com>
2022-05-03 23:29:27 +0100

    add function and evenlistener

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,22 @@ document.querySelector('.form-control').focus();
 
 //  Listen for submit
 let inp = document.querySelector('.form-control');
+document.querySelector('#calculate-form').addEventListener('submit',function(e){
+
+    if (inp.value == 0) {
+        alert('Number must to be bigger than 0');
+      
+        
+    } else {
+        
+        setTimeout(calculateWats , 2500);
+    //hide results if submit clickd again 
+    document.querySelector('#dayconsumpition').style.display = 'none';
+    document.querySelector('#nightconsumpition').style.display = 'none';
+    document.querySelector('#loading').style.display = 'block';
+
+    e.preventDefault();
+    }
+   
+    
+});
