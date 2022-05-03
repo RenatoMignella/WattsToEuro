5df2f0fe4273732265be118f511df6f2d22d4b0a
Renato Mignella <renatoferigato@gmail.com>
2022-05-03 23:26:29 +0100

    add dasable loading

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,5 @@
 // Keep resuls hided
 document.querySelector('#dayconsumpition').style.display = 'none';
 document.querySelector('#nightconsumpition').style.display = 'none';
+//disable loading
+document.querySelector('#loading').style.display = 'none';
