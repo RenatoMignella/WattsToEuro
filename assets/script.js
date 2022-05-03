e1a33f31c037a3f848ab922998bdb1336a93194b
Renato Mignella <renatoferigato@gmail.com>
2022-05-03 23:27:11 +0100

    add focus input

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,5 @@ document.querySelector('#dayconsumpition').style.display = 'none';
 document.querySelector('#nightconsumpition').style.display = 'none';
 //disable loading
 document.querySelector('#loading').style.display = 'none';
+// keep focus on Input (love math ref)
+document.querySelector('.form-control').focus();
